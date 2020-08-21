afbb75596c2dd3841a772e035cb60a2f9990187d
LenovaPC <LenovaPC@DESKTOP-9FDNQSH>
2020-08-21 15:01:17 +0530

    commit

---
+++ custSerializationDemo/src/custSerializationDemo/Serialization.java
@@ -0,0 +1,14 @@
+package custSerializationDemo;
+
+public class Serialization implements Account{
+
+	public Serialization() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
